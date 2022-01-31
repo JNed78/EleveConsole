37f9d7603496359dcabfbd7b9b2ec50c01b39f66
JNed78 <jng0549@gmail.com>
2022-01-31 12:08:10 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using EleveDll;
+using System;
+
+namespace EleveConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Eleve el = new Eleve("Jean ", 10, 9);
+            Eleve el2 = new Eleve("Pierre ", 5, 13);
+            Eleve el3 = new Eleve("Darren ", 15, 18);
+
+            Eleve[] tabel = new Eleve[3];
+            tabel[0] = new Eleve("toto", 12.8f, 4);
+            tabel[1] = new Eleve("tutu", 15.8f, 1);
+            tabel[2] = new Eleve("tata", 16.8f, 8);
+            tabel[3] = new Eleve("titi", 17.8f, 10);
+
+            Console.WriteLine(el.afficher());
+           
+
+
+        }
+    }
+}
